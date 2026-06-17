6a99dcf353925218f1533ddf0430bd9c1ce30a85
TheHackdes <92146054+TheHackdes@users.noreply.github.com>
2026-06-17 19:30:53 +0200

    Update environment variables for TheHacked Registry

--- site/next.config.js
+++ site/next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'TheHacked Registry',
+    description: 'The official store for TheHackdes supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://thehackdes.github.io/kasm-registry/',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
